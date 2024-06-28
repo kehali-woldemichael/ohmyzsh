8d0256b13bbe499f92c65422824cf7d4d8e1d36f
Kehali Woldemichael <kbw29@research-zjhuanglabweb-01.oit.duke.edu>
2024-06-28 17:06:03 -0400

    feat: added all submodules for initial setup of ohmyzsh

--- custom/paths.zsh
+++ custom/paths.zsh
@@ -2,6 +2,8 @@ core_bin="$HOME/Core/bin"
 local_bin="$HOME/.local/bin"
 cargo_bin="$CARGO_HOME/bin"
 pyenv_bin="$PYENV_ROOT/bin"
+git_extra_bin="$HOME/Core/apps/git-extra-commands/bin"
 typeset -U path PATH &&  
 	path=($path $core_bin $local_bin
-		$cargo_bin $pyenv_bin)
+		  $cargo_bin $pyenv_bin
+		  $git_extra_bin)
