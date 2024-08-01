d6788d3eab028b5b1f99c8893e3c64e82a59577a
Kehali Woldemichael <kbw29@research-zjhuanglabweb-01.oit.duke.edu>
2024-08-01 17:59:44 -0400

    feat: bindkey - add shortcuts for navigation

--- custom/bindkey.zsh
+++ custom/bindkey.zsh
@@ -7,8 +7,11 @@ bindkey -v
 # Cmd-Left
 bindkey -s "^[[108;9u" '^L' # cmd+l = ctrl+l (clear screen and reset prompt)
 bindkey -s "^[^?" '^W' # cmd+backspace = ctrl+d (backward kill word)
-bindkey -s "^[[115;9u" 'clear && exec $SHELL^M'
+bindkey "^[[3;3~" kill-word
+bindkey -s "˙" '^A'
+bindkey -s "¬" '^E'
 # CLI application
+bindkey -s "^[[115;9u" 'clear && exec $SHELL^M'
 bindkey -s "^[[101;9u" 'eza^M'
 bindkey -s "^[[100;9u" 'dust^M'
 bindkey -s "^[[114;9u" 'ranger^M'
@@ -17,8 +20,8 @@ bindkey -s "^[[104;9u" 'htop^M'
 
 # Tmux - have to include since tmux uses different key-codes
 bindkey -s "^[l" '^L'
-bindkey -s "^[s" 'clear && exec $SHELL^M'
 # CLI application
+bindkey -s "^[s" 'clear && exec $SHELL^M'
 bindkey -s "^[e" 'eza^M'
 bindkey -s "^[d" 'dust^M'
 bindkey -s "^[r" 'ranger^M'
